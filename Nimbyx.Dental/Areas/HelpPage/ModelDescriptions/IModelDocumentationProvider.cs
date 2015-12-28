64ab289136f60ea7a53823628e55e1d1ecfc8f91
xmione <solomiosisante@gmail.com>
2015-12-28 09:12:59 +0800

    setup EF, Web API, Models and Nimbyx Dental Web Application

---
+++ Nimbyx.Dental/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Nimbyx.Dental.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
